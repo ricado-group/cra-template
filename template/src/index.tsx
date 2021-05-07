410ba5ae601e176e61c1f3b1f334e7880648146b
Ash Neilson <ash@ricado.co.nz>
2021-05-07 12:43:43 +1200

    Removed serviceWorker from the Template

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.less";
 import App from "./App/App";
-import * as serviceWorker from "./serviceWorker";
 import AppProviders from "./context/AppProviders";
 import Bugsnag from "@bugsnag/js";
 import { ErrorBoundaryResult, defaultBugsnagConfig } from "@ricado/core-ui";
@@ -20,8 +19,3 @@ ReactDOM.render(
   </ErrorBoundary>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
